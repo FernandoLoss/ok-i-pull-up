ef266a8918bef806a02d30e076b787084ba0604e
FernandoLoss <93294836+FernandoLoss@users.noreply.github.com>
2022-05-26 19:45:57 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var player;
+var gun;
+var bullet;
+var sword;
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(1000,600);
+  
+  player= createSprite(500,300,20,20);
+
+}
+
+function draw() {
+
+  /*if(keyDown("right_arrow")){
+    Player.velocityX= -10;
+
+  }*/
+
+ /* if(keyIsDown(LEFT_ARROW)) {
+    player.x -= 13;
+  }*/
+
+ // player.velocityX= -3;
+  
+ 
+
+  drawSprites()
+}
